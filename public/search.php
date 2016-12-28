45ede4bd62d5c8aff3daea6d705b4faf9c400e1f
SanjayBabu-sopho <sanjay.ch222@gmail.com>
2016-12-28 07:52:33 +0000

    search.php is finished

---
+++ public/search.php
@@ -0,0 +1,15 @@
+<?php
+
+    require(__DIR__ . "/../includes/config.php");
+
+    // numerically indexed array of places
+    $places = [];
+    
+    
+    $places = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name) AGAINST (?)", $_GET["geo"]);
+
+    // output places as JSON (pretty-printed for debugging convenience)
+    header("Content-type: application/json");
+    print(json_encode($places, JSON_PRETTY_PRINT));
+
+?>
